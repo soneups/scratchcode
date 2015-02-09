89abf13e0d7aa04b4eb7498566dee177daf4b2c9
Gary Sone <gary.sone@bh.sone.net>
2015-02-09 18:14:58 +0000

    Create tweet_edu2_ws3_temp.py

---
+++ CCEP/tweet_edu2_ws3_temp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#
+# 9-Feb-2015 v1.0 initial script
+# this file - enos.in/ccep-tweet
+#
+
+import sys
+import time
+import socket
+import os
+
+from twython import Twython
+
+# Define location of api files - recorded in a file - avoids a GitHub slurp for API keys!
+keys_file='/home/scripts/twitter_api.key'
+
+# Read API keys from file
+with open(keys_file) as f:
+    CONSUMER_KEY = f.readline().strip("\n")
+    CONSUMER_SECRET = f.readline().strip("\n")
+    ACCESS_KEY = f.readline().strip("\n")
+    ACCESS_SECRET = f.readline().strip("\n")
+
+# Post tweet
+api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
+#api.update_status(status=str(time.strftime("%d/%m/%Y"))+"@"+str(time.strftime("%H:%M:%S"))+' - user: ' + str(wai) + ', host: ' +str(hn)+ ', ip: ' +str(ip)+' ('+sys.argv[1]+')')
+api.update_status(status="@yragstem - twitter bot tweet!...")
