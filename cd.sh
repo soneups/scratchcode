4f42b8b6c9962e01a6d7fead161a08510a2ed82f
Gary Sone <gary.sone@bh.sone.net>
2015-10-08 13:20:36 +0100

    Update cd.sh

--- cd.sh
+++ cd.sh
@@ -1,2 +1,3 @@
-this file --
+#!/bin/bash -x
+# this file -- enos.in/cd -- https://raw.githubusercontent.com/soneups/scratchcode/master/cd.sh
 echo $(( $(( $( date +%s ) - $( date -d "1989-11-29" +%s ) )) / 86400 )) # Calculate how many days old you are. Uses GNU syntax.
