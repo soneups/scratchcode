fd1dc81bf499a61c6e62ad5786f2b9817a390939
Gary Sone <gary.sone@bh.sone.net>
2021-01-30 11:39:19 +0000

    Update cd.sh
    updated to run on ZSH under MacOS

--- cd.sh
+++ cd.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -x
 # this file -- enos.in/cd -- https://raw.githubusercontent.com/soneups/scratchcode/master/cd.sh
-echo $(( $(( $( date +%s ) - $( date -d "1989-11-29" +%s ) )) / 86400 )) # Calculate how many days old you are. Uses GNU syntax.
+A="2021-30-1"; B="2021-10-5";
+echo $(((`date -jf %Y-%d-%m $B +%s` - `date -jf %Y-%d-%m $A +%s`)/86400))
